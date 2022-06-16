44dd406080a896853952db5855718d026e905be1
Franco Fernandez <francofernandez@192.168.1.5>
2022-06-16 14:37:14 -0300

    firebase batch updates

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,6 @@
 export default function Loader (){
     return(
         <div className='loader-container'>
-            {/* <div className='loader-1'></div>
-            <div className='loader-2'></div> */}
             <div className='loader'></div>
         </div>
     )
